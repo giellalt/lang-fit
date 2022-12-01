86cf62a4c477b043416828d3807dbf85265792cb
Gunnar Eriksson <gueriksson@gmail.com>
2022-12-01 16:06:57 +0000

    check-missing.sh: check for required argument and some documentation

--- devtools/check-missing.sh
+++ devtools/check-missing.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+##
+## Script that returns the stems/$TYPE.lexc entries for the missing
+## lemmas together with a count of the continuation lexica used for
+## these lemmas.
+##
+
+possible_values=`ls test/src/morphology/missing_*_lemmas.hfst.txt | cut -f2 -d'_' `
+call="Call:\n   "$0" [ ${possible_values} ]";
+
+if [ $# = 0 ]; then
+    echo ""
+    echo "Exiting: Missing type argument, c.f. below:";
+    echo $call
+    exit 1
+fi
+
 TYPE=$1;
 
 MISSING=test/src/morphology/missing_${TYPE}_lemmas.hfst.txt
@@ -9,8 +25,8 @@ cat $MISSING | awk '{print ("^" $1 ":")}' > $FILTER
 
 echo ""
 echo '#### STEMS:'
-egrep -f $FILTER src/fst/stems/${TYPE}.lexc 
+egrep --color -f $FILTER src/fst/stems/${TYPE}.lexc 
 
 echo ""
-echo '#### AFFIXES:'
+echo '#### CONTINUATION LEXICA:'
 egrep -f $FILTER src/fst/stems/${TYPE}.lexc | awk '{print $2}' | sort | uniq -c | sort -nr --key=1,1
