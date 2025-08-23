7f473240dc7e133d95f46af6dfde509617f919ef
Trondtr <trond.trosterud@uit.no>
2025-08-23 08:15:46 +0300

    paths

--- devtools/tag_test.sh
+++ devtools/tag_test.sh
@@ -1,19 +1,19 @@
 #shell script to see if there are tags which are not declared in root.lexc or if tags are misspelled
 
 echo 'Are there tags not declared in root.lexc or misspelled?'
-cat src/fst/clitics.lexc src/fst/compounding.lexc  src/fst/affixes/*lexc src/fst/stems/*lexc src/fst/generated_files/*lexc |cut -d '!' -f1 |grep ' ;' | cut -d ':' -f1 |tr -s ' ' |sed 's/^ //' | cut -d ' ' -f1 |sed 's/+/¢+/g' |sed 's/@/¢@/g'|tr '¢' '\n' | tr '#"%' '\n'| egrep '(\+|@)' |sort -u | egrep -v '^(\+|\+\/\-|\+Cmp\-|\@0)$' > lexctags
+cat src/fst/morphology/clitics.lexc src/fst/morphology/compounding.lexc  src/fst/morphology/affixes/*lexc src/fst/morphology/stems/*lexc src/fst/morphology/generated_files/*lexc |cut -d '!' -f1 |grep ' ;' | cut -d ':' -f1 |tr -s ' ' |sed 's/^ //' | cut -d ' ' -f1 |sed 's/+/¢+/g' |sed 's/@/¢@/g'|tr '¢' '\n' | tr '#"%' '\n'| egrep '(\+|@)' |sort -u | egrep -v '^(\+|\+\/\-|\+Cmp\-|\@0)$' > lexctags
 
-cat src/fst/root.lexc |cut -d '!' -f1 |cut -d ':' -f1 |sed 's/+/¢+/g'|sed 's/@/¢@/g' |tr '¢' '\n' | egrep '(\+|@)' |tr -d ' ' | tr -d '\t'|sort -u > roottags
+cat src/fst/morphology/root.lexc |cut -d '!' -f1 |cut -d ':' -f1 |sed 's/+/¢+/g'|sed 's/@/¢@/g' |tr '¢' '\n' | egrep '(\+|@)' |tr -d ' ' | tr -d '\t'|sort -u > roottags
 
 echo 'Have a look at these:'
 
 comm -23 lexctags roottags 
 
 echo 'Checking for double semicolon in stem files:'
-cat src/fst/stems/*lexc |cut -d '!' -f1 |grep ';.*;'
+cat src/fst/morphology/stems/*lexc |cut -d '!' -f1 |grep ';.*;'
 
 echo 'Checking for missing Der-tags:'
-cat src/fst/stems/*lexc |cut -d '!' -f1 |grep '\+Der/.*;' |egrep -v 'Der([1234]|\+)'
+cat src/fst/morphology/stems/*lexc |cut -d '!' -f1 |grep '\+Der/.*;' |egrep -v 'Der([1234]|\+)'
 
 
 echo 'checked'
